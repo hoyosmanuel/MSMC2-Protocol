50e21a3bdccb596cf2fdfd0a02e0c3301d017a86
Manuel Hoyos <101018625+hoyosmanuel@users.noreply.github.com>
2026-04-08 11:58:58 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,1 +1,19 @@
+project = "MSMC2 Protocol"
+author = "Manuel Hoyos"
+release = "0.1"
 
+extensions = ["myst_parser"]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+html_theme = "sphinx_rtd_theme"
+
+html_context = {
+    "display_github": True,
+    "github_user": "hoyosmanuel",
+    "github_repo": "MSMC2-Protocol",
+    "github_version": "main",
+}
