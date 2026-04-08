da13e928bdc181633102e825bc8a9f338365d4f2
Manuel Hoyos <101018625+hoyosmanuel@users.noreply.github.com>
2026-04-08 11:17:09 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,4 @@
 project = 'MSMC2 Protocol'
-copyright = '2026, Manuel Hoyos'
 author = 'Manuel Hoyos'
 release = '0.1'
 
@@ -10,19 +9,11 @@ source_suffix = {
     ".md": "markdown",
 }
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
 html_context = {
     "display_github": True,
     "github_user": "hoyosmanuel",
     "github_repo": "MSMC2-Protocol",
     "github_version": "main",
-}
-
-html_theme_options = {
-    "display_version": True,
 }
